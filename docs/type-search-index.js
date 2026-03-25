100dacf5c193678ddba05808dc3bc088a9e135da
Yoshiyuki Shimada <yoshiyuki.s@snow-link-drone.jp>
2026-03-25 23:30:05 +0900

    Add Javadoc documentation

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BingoCard"},{"p":"<Unnamed>","l":"BingoCell"},{"p":"<Unnamed>","l":"GameSystem"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"RandomGenerator"}];updateSearchResults();
